2f1279d537feaca83f2243407e238f5e5aa15211
Harsh Vyapari <harshv1741@gmail.com>
2023-07-08 21:17:17 +0530

    feat: Question 2 Requirements is fulfilled

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -3,34 +3,42 @@ import { restaurantsData } from "../db/database"
 
 function List({ search_id }) {
   return (
-    <div>
-      {console.log(search_id)}
+    <div >
       {
         restaurantsData.filter((item) => item.cuisine_id === search_id).map((restaurant) => (
           <div key={restaurant.id}>
             <h1>Dishes By {restaurant.name}</h1>
-            {
-              restaurant.menu.map((item, index) => {
-                return (
-                  <div key={index} className="MenuItemCard">
-                    <img className="ImageCard" src={item.imgSrc} alt={item.name} width="200px" />
-                    <span style={{
-                      fontSize: "20px",
-                      fontWeight: "500",
-                    }}
-                    >{item.name}</span>
-                    <br />
-                    <span>Rs. {item.price}</span>
-                    <br />
-                    <span>{restaurant.name}</span>
-                  </div>
-                )
-              })
-            }
+            <div style={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "center",
+              alignItems: "center",
+            }}>
+              {
+                restaurant.menu.map((item, index) => {
+                  return (
+                    <div style={{
+                      margin: "1rem",
+                    }} key={index} className="MenuItemCard">
+                      <img className="ImageCard" src={item.imgSrc} alt={item.name} width="200px" />
+                      <span style={{
+                        fontSize: "20px",
+                        fontWeight: "500",
+                      }}
+                      >{item.name}</span>
+                      <br />
+                      <span>Rs. {item.price}</span>
+                      <br />
+                      <span>{restaurant.name}</span>
+                    </div>
+                  )
+                })
+              }
+            </div>
           </div>
         ))
       }
-    </div >
+    </div>
   )
 }
 
