53d9866f7c292f76d9657367cf834f50a66c8c98
Harsh Vyapari <harshv1741@gmail.com>
2023-07-08 19:58:51 +0530

    feat: Designing Elements for Question 1

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,33 @@
 function Home() {
   return (
     <div className="App">
-      <h1>This is Home Page</h1>
+      <div
+        style={{
+          textAlign: "center",
+        }}
+        className="Title">
+        <span style={{
+          fontSize: "35px",
+          fontWeight: "500",
+        }}>
+          Food Ordering App
+          <br />
+        </span>
+
+        <br />
+        <span style={{
+          fontSize: "25px",
+          fontWeight: "500",
+        }}>
+          Select Your Cuisine
+          <br />
+          <button className="MenuButton">Italian</button>
+          <button className="MenuButton">Mexican</button>
+          <button className="MenuButton">Chinese</button>
+          <button className="MenuButton">Indian</button>
+        </span>
+        <br />
+      </div>
     </div>
   );
 }
